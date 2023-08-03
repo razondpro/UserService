fb2b07a8ad7f890ff5efbe8ce78c876ba9c560bc
Razon Miah <miahrazon@gmail.com>
2023-08-03 23:03:04 +0200

    test the core  with xunit

---
+++ tests/Modules/User/Domain/ValueObjects/UserName.cs
@@ -0,0 +1,46 @@
+using UserService.Modules.User.Domain.Exceptions;
+using UserService.Modules.User.Domain.ValueObjects;
+using Xunit;
+
+namespace UserService.Tests.Modules.User.Domain.Events;
+
+public class UserNameTests
+{
+    [Theory]
+    [InlineData("john123")]
+    [InlineData("user1234")]
+    public void Create_ValidUserName_ReturnsUserNameInstance(string userName)
+    {
+        var result = UserName.Create(userName);
+        Assert.NotNull(result);
+        Assert.Equal(userName, result.Value);
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData(" ")]
+    [InlineData(null)]
+    public void Create_NullOrWhiteSpaceUserName_ThrowsInvalidUserNameException(string userName)
+    {
+        var ex = Assert.Throws<InvalidUserNameException>(() => UserName.Create(userName));
+        Assert.Equal("User name is required", ex.Message);
+    }
+
+    [Theory]
+    [InlineData("ab")]
+    [InlineData("usernameusernameusernameusernameusernameusernameusernameusername")]
+    public void Create_InvalidLengthUserName_ThrowsInvalidUserNameException(string userName)
+    {
+        var ex = Assert.Throws<InvalidUserNameException>(() => UserName.Create(userName));
+        Assert.Equal($"User name must be between {UserName.MinLength} and {UserName.MaxLength} characters long", ex.Message);
+    }
+
+    [Theory]
+    [InlineData("user@123")]
+    [InlineData("username!")]
+    public void Create_InvalidCharactersInUserName_ThrowsInvalidUserNameException(string userName)
+    {
+        var ex = Assert.Throws<InvalidUserNameException>(() => UserName.Create(userName));
+        Assert.Equal("User name must contain only letters and numbers", ex.Message);
+    }
+}
